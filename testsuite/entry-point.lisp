5fde62d7376278610a160e54d3d4fb0b06ea16c2
Michaël Le Barbier <michael.le-barbier@melusina.org>
2024-06-26 12:44:53 +0200

    Activate unit tests for LOCAL-FILESYSTEM-SUBTREE

--- testsuite/entry-point.lisp
+++ testsuite/entry-point.lisp
@@ -19,7 +19,6 @@
   (project-unit-test)
   (steward-unit-test)
   (simulator-unit-test)
-  #+org.melusina.cid/poc
   (local-filesystem-subtree-unit-test)
   #+org.melusina.cid/poc
   (docker-engine-unit-test))
